8e572bf02d8a39a0e7c311bdca8f2e124141bd7f
Natalia Nosova <nat27555@mail.ru>
2023-10-18 21:29:15 +0300

    Added Seminar9Houm

---
+++ Seminar9Houm/Program.cs
@@ -0,0 +1,64 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"  
+*/
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int M = 1;
+NaturalToLow(N, M);
+
+
+void NaturalToLow(int N, int M)
+{
+    if (M > N)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(N, M + 1);
+        Console.Write(M + " ");
+    }
+}
+
+
+/*Задача 66: Задайте значения M и N. Напишите программу,
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+ 
+ Console.Write("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int summa(int M , int N)
+{
+if (M==N)
+return N;
+else return M + summa(M + 1, N);
+}
+Console.Write($"Сумма равна {summa(M ,N)}")
+
+/*Задача 68: Напишите программу вычисления функции Аккермана 
+с помощью рекурсии. Даны два неотрицательных числа m и n.
+ m = 2, n = 3 -> A(m,n) = 9
+ m = 3, n = 2 -> A(m,n) = 29
+ */
+
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m - 1, 1);
+else return akkerman(m - 1, akkerman(m, n - 1));
+}
+
+Console.Write($"Функция Аккермана равно {akkerman(m, n)} ");
